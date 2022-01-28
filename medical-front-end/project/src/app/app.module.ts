a1ab737c1d571194293ee8a87d95c5ce55516c02
AmrAymanAboelanwar <77896593+AmrAymanAboelanwar@users.noreply.github.com>
2022-01-28 09:26:06 +0200

    nav and home and footer componenet

--- medical-front-end/project/src/app/app.module.ts
+++ medical-front-end/project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    FooterComponent,
+  
+  
   ],
   imports: [
     BrowserModule,
